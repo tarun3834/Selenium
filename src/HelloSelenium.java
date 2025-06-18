a9fc02ad6fd024378b9ad105bab62b6ef0297f1d
unknown <you@example.com>
2025-06-18 09:28:42 +0530

    First Commit into bitbucket

---
+++ src/HelloSelenium.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class HelloSelenium {
+    @Test
+    public static void main(String[] args) {
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriv er.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        System.out.println("Title: " + driver.getTitle());
+        System.out.println("URL" + driver.getCurrentUrl());
+        driver.close();
+    }
+}
