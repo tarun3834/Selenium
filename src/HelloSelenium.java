128877ac243514660b537ccce7e29e3d4a373f82
tarun3834 <tarun3834@gmail.com>
2025-08-10 12:08:45 +0530

    second

--- src/HelloSelenium.java
+++ src/HelloSelenium.java
@@ -3,22 +3,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.SQLOutput;
-
-
 public class HelloSelenium {
+
     @Test
-    public static void main(String[] args) {
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriv er.exe");
+    public void testLogin() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
         System.out.println("Title: " + driver.getTitle());
-        System.out.println("URL" + driver.getCurrentUrl());
-        System.out.println("Hello world");
-        driver.close();
+        System.out.println("URL: " + driver.getCurrentUrl());
+
+        driver.quit();
     }
 }
