dc3cfb5f1edfc9db3649b9a60912a524842c5850
tarun3834 <tarun3834@gmail.com>
2025-08-24 21:20:01 +0530

    Fix pom.xml and update HelloSelenium test

--- src/test/java/HelloSelenium.java
+++ src/test/java/HelloSelenium.java
@@ -1,23 +1,43 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HelloSelenium {
 
     @Test
     public void testLogin() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
+        // Setup ChromeDriver automatically
+        WebDriverManager.chromedriver().setup();
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        // Configure Chrome options for headless mode (good for CI)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
 
-        System.out.println("Title: " + driver.getTitle());
-        System.out.println("URL: " + driver.getCurrentUrl());
+        WebDriver driver = new ChromeDriver(options);
 
-        driver.quit();
+        try {
+            driver.get("https://www.saucedemo.com/");
+
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
+
+            System.out.println("Title: " + driver.getTitle());
+            System.out.println("URL: " + driver.getCurrentUrl());
+
+            // Basic assertion to verify login worked
+            assert driver.getCurrentUrl().contains("inventory");
+            System.out.println("Login test passed!");
+
+        } finally {
+            driver.quit();
+        }
     }
 }
